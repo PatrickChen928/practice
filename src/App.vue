4ada8898815e8ca23fe8ebf2487e916588afdec0
cyly <786156072@qq.com>
2022-07-18 23:08:59 +0800

    build: dist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
+<script setup>
+import Index from './pages/index.vue'
+</script>
+
 <template>
   <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
-    <router-view />
+    <Index />
     <Footer />
   </main>
 </template>
